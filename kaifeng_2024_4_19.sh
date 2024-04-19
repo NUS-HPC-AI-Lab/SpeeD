14d95e86844589ee8a58542b918f4ad4564a61cc
周钰坤 <90625606+1zeryu@users.noreply.github.com>
2024-04-19 13:01:43 +0800

    Update kaifeng_2024_4_19.sh

--- kaifeng_2024_4_19.sh
+++ kaifeng_2024_4_19.sh
@@ -17,7 +17,7 @@ torchrun --nproc_per_node=8 main.py -c comparison/ffhq/min_snr.yaml -p inference
 torchrun --nproc_per_node=8 main.py -c comparison/ffhq/lognorm.yaml -p inference data.dataset.root=$FFHQ_DIR experiment_dir=$experiment_dir/lognorm ckpt_path=$experiment_dir/lognorm/train/checkpoints/0050000.pt
 torchrun --nproc_per_node=8 main.py -c comparison/ffhq/clts.yaml -p inference data.dataset.root=$FFHQ_DIR experiment_dir=$experiment_dir/clts ckpt_path=$experiment_dir/clts/train/checkpoints/0050000.pt
 
-python -m pytorch_fid $experiment_dir/p2/inference/0050000 $FFHQ_DIR
-python -m pytorch_fid $experiment_dir/min_snr/inference/0050000 $FFHQ_DIR
-python -m pytorch_fid $experiment_dir/lognorm/inference/0050000 $FFHQ_DIR
-python -m pytorch_fid $experiment_dir/clts/inference/0050000 $FFHQ_DIR
+python -m pytorch_fid $experiment_dir/p2/inference/0050000 $FFHQ_DIR > $experiment_dir/p2/inference/0050000.txt
+python -m pytorch_fid $experiment_dir/min_snr/inference/0050000 $FFHQ_DIR $experiment_dir/min_snr/inference/0050000.txt
+python -m pytorch_fid $experiment_dir/lognorm/inference/0050000 $FFHQ_DIR $experiment_dir/lognorm/inference/0050000.txt
+python -m pytorch_fid $experiment_dir/clts/inference/0050000 $FFHQ_DIR $experiment_dir/clts/inference/0050000.txt
